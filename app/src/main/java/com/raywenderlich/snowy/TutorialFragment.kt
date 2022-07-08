fe6238d9217a027ab4cb6a9759d9d63de9dca0c2
matheus lima <matheusmt31@gmail.com>
2022-07-07 22:11:13 -0300

    adicionado coroutineExceptionHandler

--- app/src/main/java/com/raywenderlich/snowy/TutorialFragment.kt
+++ app/src/main/java/com/raywenderlich/snowy/TutorialFragment.kt
@@ -33,6 +33,7 @@ package com.raywenderlich.snowy
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,56 +42,73 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.raywenderlich.snowy.model.Tutorial
 import com.raywenderlich.snowy.utils.SnowFilter
+import kotlinx.android.synthetic.main.fragment_tutorial.*
 import kotlinx.coroutines.*
 import java.net.URL
 
 class TutorialFragment : Fragment() {
 
-  companion object {
+    companion object {
 
-    const val TUTORIAL_KEY = "TUTORIAL"
+        const val TUTORIAL_KEY = "TUTORIAL"
 
-    fun newInstance(tutorial: Tutorial): TutorialFragment {
-      val fragmentHome = TutorialFragment()
-      val args = Bundle()
-      args.putParcelable(TUTORIAL_KEY, tutorial)
-      fragmentHome.arguments = args
-      return fragmentHome
-    }
-  }
-
-  private val parentJob = Job()
-  private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
-  private lateinit var imageFiltered : ImageView
-  private fun getOriginalBitmapAsync(tutorial: Tutorial): Deferred<Bitmap> =
-
-    coroutineScope.async(Dispatchers.IO) {
-      URL(tutorial.url).openStream().use {
-        return@async BitmapFactory.decodeStream(it)
-      }
+        fun newInstance(tutorial: Tutorial): TutorialFragment {
+            val fragmentHome = TutorialFragment()
+            val args = Bundle()
+            args.putParcelable(TUTORIAL_KEY, tutorial)
+            fragmentHome.arguments = args
+            return fragmentHome
+        }
     }
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                            savedInstanceState: Bundle?): View? {
-    val tutorial = arguments?.getParcelable<Tutorial>(TUTORIAL_KEY) as Tutorial
-    val view = inflater.inflate(R.layout.fragment_tutorial, container, false)
-    view.findViewById<TextView>(R.id.tutorialName).text = tutorial.name
-    view.findViewById<TextView>(R.id.tutorialDesc).text = tutorial.description
-    imageFiltered = view.findViewById(R.id.snowFilterImage)
-    return view
-  }
+    private val coroutineExceptionHandler: CoroutineExceptionHandler =
+        CoroutineExceptionHandler { _, throwable ->
+            coroutineScope.launch(Dispatchers.Main) {
+                errorMessage.visibility = View.VISIBLE
+                errorMessage.text = getString(R.string.error_message)
+            }
+            GlobalScope.launch { Log.d("teste", "CoroutineExceptionHandler") }
+        }
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + coroutineExceptionHandler)
+    private lateinit var imageFiltered: ImageView
+    private fun getOriginalBitmapAsync(tutorial: Tutorial): Deferred<Bitmap> =
 
-  private fun loadSnowFilterAsync(originalBitmap: Bitmap): Deferred<Bitmap> =
-    coroutineScope.async(Dispatchers.Default) {
-      SnowFilter.applySnowEffect(originalBitmap)
+        coroutineScope.async(Dispatchers.IO) {
+            URL(tutorial.url).openStream().use {
+                return@async BitmapFactory.decodeStream(it)
+            }
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val tutorial = arguments?.getParcelable<Tutorial>(TUTORIAL_KEY) as Tutorial
+        val view = inflater.inflate(R.layout.fragment_tutorial, container, false)
+        view.findViewById<TextView>(R.id.tutorialName).text = tutorial.name
+        view.findViewById<TextView>(R.id.tutorialDesc).text = tutorial.description
+        imageFiltered = view.findViewById(R.id.snowFilterImage)
+        return view
     }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    val tutorial = arguments?.getParcelable<Tutorial>(TUTORIAL_KEY) as Tutorial
-    coroutineScope.launch(Dispatchers.Main) {
-      val bitmapEffected = SnowFilter.applySnowEffect(getOriginalBitmapAsync(tutorial).await())
-      imageFiltered.setImageBitmap(bitmapEffected)
+    private fun loadSnowFilterAsync(originalBitmap: Bitmap): Deferred<Bitmap> =
+        coroutineScope.async(Dispatchers.Default) {
+            SnowFilter.applySnowEffect(originalBitmap)
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tutorial = arguments?.getParcelable<Tutorial>(TUTORIAL_KEY) as Tutorial
+        coroutineScope.launch(Dispatchers.Main) {
+            val bitmapEffected =
+                SnowFilter.applySnowEffect(getOriginalBitmapAsync(tutorial).await())
+            imageFiltered.setImageBitmap(bitmapEffected)
+        }
     }
-  }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        parentJob.cancel()
+    }
 }
